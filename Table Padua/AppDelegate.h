53d220124c8a22fef84711ef51b887f852d8818c
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-01-22 12:22:28 -0600

    Initial Commit

---
+++ Table Padua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Table Padua
+//
+//  Created by Walter Gonzalez Domenzain on 22/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Table Padua/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table Padua
+//
+//  Created by Walter Gonzalez Domenzain on 22/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
